89eed6a56031e135f2b8b874b6d2ee73a37fad4b
akuatbekov23 <akuatbekov23@gmail.com>
2023-10-17 02:18:03 -0400

    lab3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import Kanbas from "./Kanbas";
+import Labs from "./Labs";
+import HelloWorld from "./Labs/a3/HelloWorld";
+import logo from "./logo.svg";
+// import "./App.css";
+import { HashRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
+import StateManagement from "./Lectures/StateManagement";
+
 function App() {
-   return (
-       <h1>Hello World!</h1>
-   );
+  return (
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/Labs" />} />
+
+          <Route path="/hello" element={<HelloWorld />} />
+          <Route path="/Lectures" element={<StateManagement />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
 }
+
 export default App;
