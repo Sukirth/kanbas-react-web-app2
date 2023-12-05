02b636ec68934cbc62dea366e1e9c603dd7fd1f8
akuatbekov23 <akuatbekov23@gmail.com>
2023-12-04 22:37:50 -0500

    a5

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,175 +1,180 @@
-/* eslint-disable no-use-before-define */
 /* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
-import "./ModuleList.css";
-import { BsCheckCircle, BsGripVertical, BsPlusLg, BsThreeDotsVertical } from "react-icons/bs";
+import "./modulelist.css";
+import { BsCheckCircleFill } from "react-icons/bs";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { BsPlus } from "react-icons/bs";
+import { useSelector, useDispatch } from "react-redux";
 import {
-    AiFillCaretDown,
-    AiFillCaretRight,
-    AiFillCheckCircle,
-    AiOutlinePlus,
-} from "react-icons/ai";
-import { useDispatch, useSelector } from "react-redux";
-import { addModule, deleteModule, setModule, setModules, updateModule } from "./modulesReducer";
-import { createModule, findModulesForCourse } from "./client";
+    addModule,
+    deleteModule,
+    updateModule,
+    setModule,
+    setModules,
+} from "./modulesReducer";
+import { findModulesForCourse, createModule } from "./client";
 import * as client from "./client";
+
 
 function ModuleList() {
     const { courseId } = useParams();
-    useEffect(() => {
-        findModulesForCourse(courseId)
-          .then((modules) =>
-            dispatch(setModules(modules))
-        );
-      }, [courseId, dispatch]);    
+
     const modules = useSelector((state) => state.modulesReducer.modules);
     const module = useSelector((state) => state.modulesReducer.module);
     const dispatch = useDispatch();
 
     const handleAddModule = () => {
         createModule(courseId, module).then((module) => {
-          dispatch(addModule(module));
+            dispatch(addModule(module));
         });
     };
+
     const handleDeleteModule = (moduleId) => {
         client.deleteModule(moduleId).then((status) => {
-          dispatch(deleteModule(moduleId));
+            dispatch(deleteModule(moduleId));
         });
     };
-    
+
     const handleUpdateModule = async () => {
         const status = await client.updateModule(module);
         dispatch(updateModule(module));
-    };
+      };
     
-    
-    return (
-        <div className="module-list-container">
-            <div className="course-buttons float-end">
-                <div className="btn-toolbar">
-                    <div className="btn btn-light border">Collapse All</div>
-                    <div className="btn btn-light border">View Progress</div>
-                    <div className="dropdown">
-                        <button
-                            className="btn btn-light border dropdown-toggle"
-                            type="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                        >
-                            <BsCheckCircle className="publish-icon" />
-                            Publish All
-                        </button>
-                    </div>
-                    <div className="btn btn-danger border">
-                        <BsPlusLg />
-                        Modules
-                    </div>
-                    <div className="btn btn-light border">
-                        <BsThreeDotsVertical />
-                    </div>
-                </div>
-            </div>
-            <br />
-            <br />
-            <hr />
+      useEffect(() => {
+        findModulesForCourse(courseId)
+            .then((modules) =>
+                dispatch(setModules(modules))
+            );
+    }, [courseId]);
 
-            <ul className="list-group modules-list">
-                <li className="list-group-item">
-                    <h5>
-                        Module
-                    </h5>
-                    <form>
-                        <div className="form-group">
-                            <input
-                                className="form-control"
-                                value={module.name}
-                                onChange={(e) =>
-                                    dispatch(
-                                        setModule({
-                                            ...module,
-                                            name: e.target.value,
-                                        })
-                                    )
-                                }
-                            />
-                            <textarea
-                                className="form-control mt-2"
-                                value={module.description}
-                                onChange={(e) =>
-                                    dispatch(
-                                        setModule({
-                                            ...module,
-                                            description: e.target.value,
-                                        })
-                                    )
-                                }
-                            />
-                            <div className="mt-2">
-                                <button
-                                    className="btn btn-light border-dark mx-1 btn-sm"
-                                    // onClick={() =>
-                                    //     dispatch(addModule({ ...module, course: courseId }))
-                                    // }
-                                    onClick={handleAddModule}
-                                >
-                                    Add
-                                </button>
-                                <button
-                                    className="btn btn-light border-dark mx-1 btn-sm"
-                                    // onClick={() => dispatch(updateModule(module))}
-                                    onClick={() => handleUpdateModule()}
-                                >
-                                    Update
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    return (
+        <div className="home-modules">
+            <div >
+                {/* Expand Options Button */}
+                <button type="button" class="btn btn-light float-end">
+                    <BsThreeDotsVertical />
+                </button>
+
+                {/* Add Module Button */}
+                <button type="button" class="btn btn-danger float-end">
+                    <BsPlus size={20} /> Module
+                </button>
+
+                {/* Publish All Button */}
+                <div class="dropdown">
+                    <button
+                        class="btn btn-light dropdown-toggle float-end"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >
+                        <BsCheckCircleFill color={"green"} /> Publish All
+                    </button>
+                </div>
+
+                {/* View Progress Button */}
+                <button type="button" class="btn btn-light float-end">
+                    View Progress
+                </button>
+
+                {/* Collapse All Button */}
+                <button type="button" class="btn btn-light float-end">
+                    Collapse All
+                </button>
+                <br />
+                <br />
+                <hr />
+                <br />
+
+            </div>
+            <ul className="list-group home-modules">
+                {/* The Title of the Modules */}
+                <li class="list-group-item d-flex justify-content-between align-items-center list-group-item-secondary module-title">
+                    <span class="module-title">Week 0 - INTRO</span>
+                    <BsThreeDotsVertical />
                 </li>
 
+                {/* Add or Update Module and Description*/}
+                <li className="list-group-item">
+                    <div className="row d-flex justify-content-between">
+                        <div className="col-9 edit-module-box">
+                            <input value={module.name}
+                                onChange={(e) => dispatch(setModule({ ...module, name: e.target.value }))}
+                            />
+                            <textarea value={module.description}
+                                onChange={(e) => dispatch(setModule({ ...module, description: e.target.value }))}
+                            />
+                        </div>
+
+                        <div className="col-3 justify-content-end add-update-btns">
+                            <button
+                                className="btn btn-danger"
+                                onClick={handleAddModule}>
+                                Add </button>
+                            <button
+                                className="btn btn-primary"
+                                onClick={handleUpdateModule}>
+                                Update </button>
+                        </div>
+                    </div>
+                </li>
+
+                {/* Module List For the Week*/}
                 {modules
                     .filter((module) => module.course === courseId)
                     .map((module, index) => (
-                        <li
-                            key={index}
-                            className="list-group-item border list-group-item-light rounded-0"
-                        >
-                            <div className="module-list-content">
-                                <div>
-                                    <BsGripVertical size={24} />
-                                    <AiFillCaretRight size={24} />
-                                    <span>
-                                        {module.name} - {module.description}
+                        <div>
+                            {/* Module name */}
+                            <div className="complete-modules">
+                                <li
+                                    key={index}
+                                    className="list-group-item d-flex justify-content-between align-items-center"
+                                >
+                                    <span className="module-name wd-flex-row-container">
+                                        {module.name}
                                     </span>
-                                </div>
-                                <div style={{ flexGrow: 1 }}>
-                                    <div className="float-end">
-                                        <button 
-                                            className="btn btn-light border-dark btn-sm"
-                                            // onClick={() => dispatch(setModule(module))}>
-                                            onClick={() => {handleUpdateModule();
-                                                        dispatch(setModule(module));}}>
-                                            Edit
-                                        </button>
-                                        <button 
-                                            className="btn btn-light border-dark btn-sm mx-2"
-                                            //onClick={() => dispatch(deleteModule(module._id))}>
-                                             onClick={() => handleDeleteModule(module._id)}>
-                                             
-                                            Delete
-                                        </button>
 
-                                        <AiFillCheckCircle className="module-list-icon-check" />
-                                        <AiFillCaretDown />
-                                        <span className="mx-3">
-                                            <AiOutlinePlus />
+                                    <div>
+                                        {/* Edit and Delete Button */}
+
+                                        <span className="edit-delete-btns">
+                                            <button
+                                                className="btn btn-light"
+                                                onClick={() => dispatch(setModule(module))}>
+                                                Edit
+                                            </button>
+                                            <button
+                                                className="btn btn-danger"
+                                                onClick={() => handleDeleteModule(module._id)}>
+                                                Delete
+                                            </button>
                                         </span>
+
+                                        <BsCheckCircleFill color={"green"} />
                                         <BsThreeDotsVertical />
                                     </div>
-                                </div>
+                                </li>
                             </div>
-                        </li>
+
+                            {/* Module description */}
+                            <div className="module-description">
+                                <li
+                                    key={index}
+                                    className="list-group-item d-flex justify-content-between align-items-center"
+                                >
+
+                                    {module.description}
+
+                                    <div>
+                                        <BsCheckCircleFill color={"green"} />
+                                        <BsThreeDotsVertical />
+                                    </div>
+                                </li>
+                            </div>
+                        </div>
                     ))}
             </ul>
         </div>
--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -1,90 +1,64 @@
 /* eslint-disable no-unused-vars */
 import db from "../../Kanbas/Database";
 import { Navigate, Route, Routes, useParams } from "react-router-dom";
-import { RxHamburgerMenu } from "react-icons/rx";
-import "./index.css";
+import { FaBars } from "react-icons/fa";
 import CourseNavigation from "./CourseNavigation";
 import Modules from "./Modules";
-import { BsEyeglasses } from "react-icons/bs";
+import "./index.css";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/AssignmentEditor";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
+
+const API_BASE = process.env.REACT_APP_API_BASE;
 
 function Courses() {
     const { courseId } = useParams();
-    const [course, setCourse] = useState({});
     const URL = "http://localhost:4000/api/courses";
+    const [course, setCourse] = useState({});
     const findCourseById = async (courseId) => {
-        const response = await axios.get(`${URL}/${courseId}`);
+        const response = await axios.get(
+            `${URL}/${courseId}`
+        );
         setCourse(response.data);
     };
+
     useEffect(() => {
         findCourseById(courseId);
     }, [courseId]);
 
+
+
     return (
         <div>
-            <div className="course-content">
-                <div className="course-title">
-                    <div className="breadcrumb-content">
-                        <nav aria-label="breadcrumb">
-                            <ol className="breadcrumb">
-                                <li id="breadcrumb-icon">
-                                    <RxHamburgerMenu size={24} />
-                                </li>
-                                <li className="breadcrumb-item breadcrumb-course-number">
-                                    {course.number}
-                                </li>
-                                <li className="breadcrumb-item">
-                                    <Routes>
-                                        <Route path="/" element={<span>Home</span>} />
-                                        <Route path="Home" element={<span>Home</span>} />
-                                        <Route path="Modules" element={<span>Modules</span>} />{" "}
-                                        <Route
-                                            path="Assignments"
-                                            element={<span>Assignments</span>}
-                                        />
-                                        <Route
-                                            path="Assignments/:assignmentId"
-                                            element={<span>Assignment Editor</span>}
-                                        />
-                                        <Route path="Grades" element={<span>Grades</span>} />
-                                    </Routes>
-                                </li>
-                            </ol>
-                        </nav>
-                    </div>
-                    <div style={{ flexGrow: 1 }} className="studentview-button">
-                        <div className="float-end">
-                            <div className="btn btn-light border">
-                                <BsEyeglasses size={24} className="studentview-button-icon" />
-                                Student View
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr />
-                <div className="course-nav-body">
-                    <CourseNavigation />
-                    <div className="course-body">
-                        <div className="overflow-y-auto">
-                            <Routes>
-                                <Route path="/" element={<Navigate to="Home" />} />
-                                <Route path="Home" element={<Home />} />
-                                <Route path="Modules" element={<Modules />} />{" "}
-                                <Route path="Assignments" element={<Assignments />} />
-                                <Route
-                                    path="Assignments/:assignmentId"
-                                    element={<AssignmentEditor />}
-                                />
-                                <Route path="Grades" element={<h1>Grades</h1>} />
-                            </Routes>
-                        </div>
-                    </div>
+            <div className="header">
+                <FaBars color={"darkred"} size={27} className="menu-bar" />
+            </div>
+            <hr></hr>
+
+            <CourseNavigation />
+            <div>
+                <div
+                    className="overflow-y-scroll position-fixed bottom-0 end-0"
+                    style={{
+                        left: "320px",
+                        top: "90px",
+                    }}
+                >
+                    <Routes>
+                        <Route path="/" element={<Navigate to="Home" />} />
+                        <Route path="Home" element={<Home />} />
+                        <Route path="Modules" element={<Modules />} />
+                        <Route path="Assignments" element={<Assignments />} />
+                        <Route
+                            path="Assignments/:assignmentId"
+                            element={<AssignmentEditor />} />
+                        <Route path="Grades" element={<h1>Grades</h1>} />
+                    </Routes>
                 </div>
             </div>
+
         </div>
     );
 }
