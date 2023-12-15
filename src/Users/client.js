2f4a386747d373dc1c29330579b9357f190f6954
akuatbekov23 <akuatbekov23@gmail.com>
2023-12-14 23:10:05 -0500

    linnk fix

--- src/Users/client.js
+++ src/Users/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_API = process.env.REACT_APP_BASE_API_URL || "http://localhost:4000";
+export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 export const USERS_API = `${BASE_API}/api/users`;
 
 const request = axios.create({
